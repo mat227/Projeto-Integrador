60549087b55dcd1ea7b7345c6216827f3e9c60ec
Matheus Oliveira <ra43670926828@acaonsfatima.org.br>
2024-06-11 21:46:51 -0300

    Implementação do Js

---
+++ Projeto/Erupcao/vulcao.js
@@ -0,0 +1,114 @@
+let imgInicio;
+let particulas = [];
+let botaoIniciar, botaoParar;
+let erupcaoAtiva = false;
+let chuvaAtiva = false;
+
+let chuva = [];
+let quantidade = 300;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // Carregar imagens
+  imgInicio = loadImage('eupcaofundo.jpg');
+
+  // Criar o botão para iniciar a erupção
+  botaoIniciar = createButton('Iniciar Erupção');
+  botaoIniciar.position(10, 10);
+  botaoIniciar.mousePressed(iniciarErupcao);
+
+  // Criar o botão para parar a erupção
+  botaoParar = createButton('Parar Erupção');
+  botaoParar.position(150, 10);
+  botaoParar.mousePressed(pararErupcao);
+
+  // Inicializar a chuva de lava
+  for (let i = 0; i < quantidade; i++) {
+    chuva.push({
+      x: random(-50, width),
+      y: random(height),
+      velocidade: random(5),
+      fase: random(TWO_PI) // Para movimento senoidal/two pi é o valor dobrado de pi
+    });
+  }
+}
+function iniciarErupcao() {
+  erupcaoAtiva = true;
+  chuvaAtiva = true;
+}
+
+function pararErupcao() {
+  erupcaoAtiva = false;
+  chuvaAtiva = false;
+}
+
+function draw() {
+  background(255);
+  image(imgInicio, 0, 0, width, height);
+
+  // Desenhar a chuva de lava
+  if (chuvaAtiva) {
+    for (let i = 0; i < chuva.length; i++) {
+      let gota = chuva[i];
+      let espessura = map(gota.velocidade, 0, 4, 0.5, 3);
+      let tamanho = map(gota.velocidade, 0, 4, 5, 15);
+      strokeWeight(espessura);
+      stroke(255, 100, 0); // Cor da lava
+      line(gota.x, gota.y, gota.x, gota.y + tamanho);
+      gota.x += sin(gota.fase) * 2; // Movimento senoidal
+      gota.y += gota.velocidade + 5;
+      gota.x += 1;
+
+      if (gota.y > height) {
+        gota.y = -20;
+        gota.x = random(-50, width);
+      }
+    }
+  }
+
+  // Atualizar e desenhar partículas de erupção
+  if (erupcaoAtiva) {
+    let numNovasParticulas = 50; // Número de novas partículas por quadro
+    for (let i = 0; i < numNovasParticulas; i++) {
+      let p = new Particula(width / 2, 100);
+      particulas.push(p);
+    }
+  }
+
+  for (let i = particulas.length - 1; i >= 0; i--) {
+    particulas[i].atualizar();
+    particulas[i].mostrar();
+    if (particulas[i].terminou()) {
+      particulas.splice(i, 1); // Remover partícula quando acaba
+    }
+  }
+}
+
+
+
+class Particula {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = random(-10, 10); // Distorção do eixo x de onde as partículas vão
+    this.vy = random(-5, -1); // Distorção do eixo y de onde as partículas vão
+    this.alpha = 255;
+  }
+
+  atualizar() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.alpha -= 5;
+  }
+
+  mostrar() {
+    noStroke();
+    fill(255, 100, 0, this.alpha);
+    ellipse(this.x -30, this.y + 160, 30); // Local de saida da lava
+  }
+
+  terminou() {
+    return this.alpha < 0;
+  }
+}
---
+++ Projeto/Tsunami/tsunami.js
@@ -0,0 +1,65 @@
+let contMovOndas = 0;
+let x = 0;
+let amplitudeSlider, frequenciaSlider;//sliders
+let velocidade = 3; //velocidade do tsunami
+let cidade, cidadeDestruida;//imagens
+let tsunamiAtivo = false;//verificação do tsunami
+let iniciarDesastre, paraDesastre;//botões
+
+function preload() {
+  cidade = loadImage('Captura de ecrã 2024-05-31 152118.png');  
+  cidadeDestruida = loadImage('Captura de ecrã 2024-05-31 152135.png');
+}
+function iniciarTsunami() {
+  tsunamiAtivo = true;
+}
+
+function pararTsunami() {
+  tsunamiAtivo = false;
+}
+function setup() {
+  createCanvas(600, 400);
+  amplitudeSlider = createSlider(0, 400, 200);
+  frequenciaSlider = createSlider(0, 400, 200);
+
+
+//botão que inicia o Tsunami
+  iniciarDesastre = createButton('Iniciar Tsunami');
+  iniciarDesastre.position(400, height + 30);
+  iniciarDesastre.mousePressed(iniciarTsunami);
+//botão que para o Tsunami
+  paraDesastre = createButton('Parar Tsunami');
+  paraDesastre.position(520, height + 30);
+  paraDesastre.mousePressed(pararTsunami);
+}
+
+function draw() {
+  let amplitude = amplitudeSlider.value();
+  let frequencia = frequenciaSlider.value();
+
+  if (tsunamiAtivo) {
+    image(cidadeDestruida, 0, 0, width, height);
+    desenharOndas(amplitude, frequencia);
+  } else {
+    image(cidade, 0, 0, width, height);
+  }
+}
+
+function desenharOndas(amplitude, frequencia) {
+  //desenha o Tsunami
+  translate(0, height / 2);
+  beginShape();
+  vertex(0, height / 2);
+  fill(0, 0, 255);
+  for (let i = 0; i < width; i++) {
+    let y = amplitude * sin(TWO_PI * (i + x) / frequencia);
+    vertex(i, y);
+  }
+  vertex(width, height / 2);
+  endShape(CLOSE);
+  x += velocidade;
+  if (x > width) {
+  // Se a onda alcançar o final da tela, reinicia o deslocamento horizontal
+    x = 0;
+  }
+}
